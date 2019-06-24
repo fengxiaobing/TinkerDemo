d6ad0ebf970e2d77d6c043bb27e03fc604be7810
fxb <516668646@qq.com>
2019-06-24 17:20:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fxb/tinkerdemo/TinkerManager.java
@@ -0,0 +1,50 @@
+package com.example.fxb.tinkerdemo;
+
+import android.content.Context;
+
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.ApplicationLike;
+
+/**
+ * Created by fxb on 2019/6/24.
+ * 对Tinker的所有APi进行封装
+ */
+
+public class TinkerManager {
+    private static boolean isInstalled = false;
+
+    private static ApplicationLike mAppLike;
+
+
+    /**
+     * 初始化
+     * @param applicationLike
+     */
+    private static void installTinker(ApplicationLike applicationLike){
+        mAppLike = applicationLike;
+        if(isInstalled){
+            return;
+        }
+        TinkerInstaller.install(mAppLike);//完成初始化
+        isInstalled = true;
+    }
+
+    private static void loadPath(String patch){
+       if(Tinker.isTinkerInstalled()){
+           TinkerInstaller.onReceiveUpgradePatch( getApplicationContext(),patch);
+       }
+    }
+
+    /**
+     * 通过ApplicationLike获取Context
+     * @return
+     */
+    private static Context getApplicationContext(){
+        if(mAppLike!=null){
+            return  mAppLike.getApplication().getApplicationContext();
+        }
+        return null;
+    }
+
+}
